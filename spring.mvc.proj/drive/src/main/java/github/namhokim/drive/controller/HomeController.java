0bcfa895a2212565b7bab754b3801b73bac9a769
Namho Kim <namhokim@sindoh.com>
2015-03-03 19:39:24 +0900

    1. Root directory auto creation 2. path to C:\drive

--- spring.mvc.proj/drive/src/main/java/github/namhokim/drive/controller/HomeController.java
+++ spring.mvc.proj/drive/src/main/java/github/namhokim/drive/controller/HomeController.java
@@ -1,9 +1,13 @@
 package github.namhokim.drive.controller;
 
+import java.io.File;
 import java.util.Locale;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +21,9 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Inject
+	private FileSystemResource fsResource;
+	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -24,7 +31,21 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.debug("Welcome home! The client locale is {}.", locale);
 		
+		createRootDriveForContentPath();
+		
 		return "home";
+	}
+
+	private void createRootDriveForContentPath() {
+		File theDir = new File(fsResource.getPath());
+		if ( HasDirectory(theDir) == false) {
+			boolean succeed = theDir.mkdirs();
+			logger.info("Create directory {} was {}", theDir.getAbsolutePath(), succeed);
+		}
+	}
+
+	private boolean HasDirectory(File theDir) {
+		return theDir.exists() && theDir.isDirectory();
 	}
 	
 }
