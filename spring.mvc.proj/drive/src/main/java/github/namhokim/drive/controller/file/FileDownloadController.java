08420eca4da51076cebd6201f7f71f188e2ee947
Namho Kim <namhokim@sindoh.com>
2015-03-10 11:00:49 +0900

    [refactoring] extract to method: getContentDisposition()

--- spring.mvc.proj/drive/src/main/java/github/namhokim/drive/controller/file/FileDownloadController.java
+++ spring.mvc.proj/drive/src/main/java/github/namhokim/drive/controller/file/FileDownloadController.java
@@ -1,6 +1,7 @@
 package github.namhokim.drive.controller.file;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 
 import javax.inject.Inject;
@@ -34,20 +35,28 @@ private static final Logger logger = LoggerFactory.getLogger(FileDownloadControl
 	public ResponseEntity<InputStreamResource> download(HttpServletRequest request, @RequestParam("filename") String filename) throws IOException {
 		
 		logger.info("{} download by {}", filename, request.getRemoteAddr());
-
-		// Java의 java.net.URLEncoder는 공백문자를 %20가 아닌 +로 인코딩한다.
-		// 만약 +로 Content-Disposition를 반환하면 공백이 +로 바뀌어 버린다. 
-		String encordedFilename = URLEncoder.encode(filename,"UTF-8").replace("+", "%20");
 		
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.setContentType(MediaType.APPLICATION_OCTET_STREAM);
-		// See. RFC-6266, http://www.rfc-base.org/txt/rfc-6266.txt
-		responseHeaders.set("Content-Disposition",
-				"attachment;filename=" + encordedFilename + ";filename*= UTF-8''" + encordedFilename);
+		responseHeaders.set("Content-Disposition", getContentDisposition(filename));
 		responseHeaders.set("Content-Transfer-Encoding", "binary");
 		return new ResponseEntity<InputStreamResource>(getFileContent(filename), responseHeaders, HttpStatus.OK);
 	}
 	
+	private String getContentDisposition(String filename) throws UnsupportedEncodingException {
+		// Java의 java.net.URLEncoder는 공백문자를 %20가 아닌 +로 인코딩한다.
+		// 만약 +로 Content-Disposition를 반환하면 공백이 +로 바뀌어 버린다. 
+		String encordedFilename = URLEncoder.encode(filename,"UTF-8").replace("+", "%20");
+		StringBuilder sb = new StringBuilder(256);
+		
+		// See. RFC-6266, http://www.rfc-base.org/txt/rfc-6266.txt
+		sb.append("attachment;filename=");
+		sb.append(encordedFilename);
+		sb.append(";filename*= UTF-8''");
+		sb.append(encordedFilename);
+		return sb.toString();
+	}
+
 	private InputStreamResource getFileContent(String filename) throws IOException {
 		String path = String.format("%s%s", fsResource.getPath(), filename);
 		FileSystemResource resource = new FileSystemResource(path);
